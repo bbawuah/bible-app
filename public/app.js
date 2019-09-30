ba9ff9757dc4b5c06750cfde51f4db0bf16c69de
Brian Bawuah <brianbawuah@icloud.com>
2019-09-30 15:29:11 +0200

    new design

--- public/app.js
+++ public/app.js
@@ -5,9 +5,9 @@ const verse = document.querySelector('.verse');
 const text = document.querySelector('.text');
 const title = document.querySelector('.title');
 const subtitle = document.querySelector('.subtitle');
-const nav = document.querySelector('.nav');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
+const nav = document.querySelectorAll('.nav');
+const prevBtn = document.querySelectorAll('.prev');
+const nextBtn = document.querySelectorAll('.next');
 const loader = document.querySelector('.gooey');
 
 const btn = document.querySelector('button');
@@ -19,7 +19,10 @@ btn.addEventListener('click', (e) => {
   title.textContent = '';
   subtitle.textContent = '';
   text.innerHTML = '';
-  nav.style.visibility = 'hidden';
+  nav.forEach((btn) => {
+    btn.style.visibility = 'hidden';
+    btn.style.opacity = '0';
+  })
   loader.style.visibility = 'visible';
 
   if (book.value && number.value && !verse.value) {
@@ -30,13 +33,15 @@ btn.addEventListener('click', (e) => {
         }
 
 
-        loader.style.visibility = 'hidden';
         title.textContent = data.chapterData.data.reference;
         subtitle.textContent = data.bookData.data.nameLong;
         text.insertAdjacentHTML('beforeend', data.chapterData.data.content);
 
         //buttons
-        nav.style.visibility = 'visible';
+        nav.forEach((btn) => {
+          btn.style.visibility = 'visible';
+          btn.style.opacity = '1';
+        })
 
       })
     });
@@ -52,7 +57,11 @@ btn.addEventListener('click', (e) => {
         subtitle.textContent = data.bookData.data.nameLong;
         text.insertAdjacentHTML('beforeend', data.verseData.data.content);
 
-        nav.style.visibility = 'visible';
+        nav.forEach((btn) => {
+          btn.style.visibility = 'visible';
+          btn.style.opacity = '1';
+        })
+
 
       })
     });
@@ -61,110 +70,138 @@ btn.addEventListener('click', (e) => {
 })
 
 
-prevBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+prevBtn.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-  title.textContent = '';
-  subtitle.textContent = '';
-  text.innerHTML = '';
-  nav.style.visibility = 'hidden';
-  loader.style.visibility = 'visible';
+    title.textContent = '';
+    subtitle.textContent = '';
+    text.innerHTML = '';
 
-  if (!verse.value) {
-    number.value = parseInt(number.value) - 1;
-    fetch(`/bible?book=${book.value}&chapter=${number.value}`).then((response) => {
-      response.json().then((data) => {
-        if (data.err) {
-          title.textContent = data.err;
-        }
-
-        loader.style.visibility = 'hidden';
-        title.textContent = data.chapterData.data.reference;
-        subtitle.textContent = data.bookData.data.nameLong;
-        text.insertAdjacentHTML('beforeend', data.chapterData.data.content);
-
-        //buttons
-        nav.style.visibility = 'visible';
-      });
+    nav.forEach((btn) => {
+      btn.style.visibility = 'hidden';
+      btn.style.opacity = '0';
     })
-  } else {
-    verse.value = parseInt(verse.value) - 1;
 
-    fetch(`/bible?book=${book.value}&chapter=${number.value}&verse=${verse.value}`).then((response) => {
-      response.json().then((data) => {
-        if (data.err) {
-          title.textContent = data.err;
-        }
-        
+    loader.style.visibility = 'visible';
 
-        loader.style.visibility = 'hidden';
-        title.textContent = data.verseData.data.reference;
-        subtitle.textContent = data.bookData.data.nameLong;
-        text.insertAdjacentHTML('beforeend', data.verseData.data.content);
+    if (!verse.value) {
+      number.value = parseInt(number.value) - 1;
+      fetch(`/bible?book=${book.value}&chapter=${number.value}`).then((response) => {
+        response.json().then((data) => {
+          if (data.err) {
+            title.textContent = data.err;
+          }
 
-        //buttons
-        nav.style.visibility = 'visible';
 
+          title.textContent = data.chapterData.data.reference;
+          subtitle.textContent = data.bookData.data.nameLong;
+          text.insertAdjacentHTML('beforeend', data.chapterData.data.content);
+
+          //buttons
+          
+
+          nav.forEach((btn) => {
+            btn.style.visibility = 'visible';
+            btn.style.opacity = '1';
+          })
+
+        });
       })
-    })
+    } else {
+      verse.value = parseInt(verse.value) - 1;
+
+      fetch(`/bible?book=${book.value}&chapter=${number.value}&verse=${verse.value}`).then((response) => {
+        response.json().then((data) => {
+          if (data.err) {
+            title.textContent = data.err;
+          }
 
 
-  }
-})
 
+          title.textContent = data.verseData.data.reference;
+          subtitle.textContent = data.bookData.data.nameLong;
+          text.insertAdjacentHTML('beforeend', data.verseData.data.content);
 
-nextBtn.addEventListener('click', (e) => {
-
-
-  e.preventDefault();
-
-  title.textContent = '';
-  subtitle.textContent = '';
-  text.innerHTML = '';
-  nav.style.visibility = 'hidden';
-  loader.style.visibility = 'visible';
-
-
-  if (!verse.value) {
-    number.value = parseInt(number.value) + 1;
-    fetch(`/bible?book=${book.value}&chapter=${number.value}`).then((response) => {
-      response.json().then((data) => {
-        if (data.err) {
-          title.textContent = data.err;
-        }
-        
-        loader.style.visibility = 'hidden';
-
-        title.textContent = data.chapterData.data.reference;
-        subtitle.textContent = data.bookData.data.nameLong;
-        text.insertAdjacentHTML('beforeend', data.chapterData.data.content);
-
-        //buttons
-        nav.style.visibility = 'visible';
-
+          //buttons
+          nav.forEach((btn) => {
+            btn.style.visibility = 'visible';
+            btn.style.opacity = '1';
+          })
+        })
       })
-    })
-  } else {
 
-    verse.value = parseInt(verse.value) + 1;
 
-    fetch(`/bible?book=${book.value}&chapter=${number.value}&verse=${verse.value}`).then((response) => {
-      response.json().then((data) => {
-        if (data.err) {
-          title.textContent = data.err;
-        }
-        
-
-        loader.style.visibility = 'hidden';
-        title.textContent = data.verseData.data.reference;
-        subtitle.textContent = data.bookData.data.nameLong;
-        text.insertAdjacentHTML('beforeend', data.verseData.data.content);
-
-        //buttons
-        nav.style.visibility = 'visible';
-
-      })
-    })
-
-  }
+    }
+  })
 });
+
+
+
+
+nextBtn.forEach((btn) => {
+
+  btn.addEventListener('click', (e) => {
+
+
+    e.preventDefault();
+
+    title.textContent = '';
+    subtitle.textContent = '';
+    text.innerHTML = '';
+    nav.forEach((btn) => {
+      btn.style.visibility = 'hidden';
+      btn.style.opacity = '0';
+    })
+    loader.style.visibility = 'visible';
+
+
+    if (!verse.value) {
+      number.value = parseInt(number.value) + 1;
+      fetch(`/bible?book=${book.value}&chapter=${number.value}`).then((response) => {
+        response.json().then((data) => {
+          if (data.err) {
+            title.textContent = data.err;
+          }
+
+
+
+          title.textContent = data.chapterData.data.reference;
+          subtitle.textContent = data.bookData.data.nameLong;
+          text.insertAdjacentHTML('beforeend', data.chapterData.data.content);
+
+          //buttons
+          nav.forEach((btn) => {
+            btn.style.visibility = 'visible';
+            btn.style.opacity = '1';
+          })
+
+        })
+      })
+    } else {
+
+      verse.value = parseInt(verse.value) + 1;
+
+      fetch(`/bible?book=${book.value}&chapter=${number.value}&verse=${verse.value}`).then((response) => {
+        response.json().then((data) => {
+          if (data.err) {
+            title.textContent = data.err;
+          }
+
+
+
+          title.textContent = data.verseData.data.reference;
+          subtitle.textContent = data.bookData.data.nameLong;
+          text.insertAdjacentHTML('beforeend', data.verseData.data.content);
+
+          //buttons
+          nav.forEach((btn) => {
+            btn.style.visibility = 'visible';
+            btn.style.opacity = '1';
+          })
+
+        })
+      })
+    }
+  });
+})
